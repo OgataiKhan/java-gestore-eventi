b608920d4fb09b36fda9d4f7355ef7bb69173126
OgataiKhan <badellino.kevin@gmail.com>
2024-05-28 09:54:31 +0200

    feat: implement event class

--- src/org/events/Event.java
+++ src/org/events/Event.java
@@ -1,2 +1,109 @@
-package org.events;public class Event {
+package org.events;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Event {
+
+    // ATTRIBUTES
+    private String title;
+    private LocalDate date;
+    private int totalSeats;
+    private int bookedSeats;
+
+    // CONSTRUCTORS
+    public Event(String title, LocalDate date, int totalSeats) {
+        this.title = validateTitle(title);
+        this.date = validateDate(date);
+        this.totalSeats = validateTotalSeats(totalSeats);
+        this.bookedSeats = 0;
+    }
+
+    // METHODS
+
+    // Validations
+    public String validateTitle(String title) throws IllegalArgumentException {
+        if(title == null || title.isEmpty()){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        return title;
+    }
+
+    public LocalDate validateDate(LocalDate date) throws IllegalArgumentException {
+        if(date.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Date must be in the future. You entered: " + date);
+        }
+        return date;
+    }
+
+    public int validateTotalSeats(int seats) throws IllegalArgumentException {
+        if(seats <= 0){
+            throw new IllegalArgumentException("Number of seats must be positive. You entered: " + seats);
+        }
+        return seats;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = validateTitle(title);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = validateDate(date);
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public int getBookedSeats() {
+        return bookedSeats;
+    }
+
+    // Other methods
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MMMM dd", Locale.ENGLISH);
+
+        return date.format(formatter) + " - " + title;
+    }
+
+    public void bookSeats(int seats) throws IllegalArgumentException {
+        if (this.date.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("You cannot book seats for a past event");
+        }
+
+        if (seats <=0){
+            throw new IllegalArgumentException("Number of seats must be positive. You entered: " + seats);
+        }
+
+        if (bookedSeats + seats > totalSeats){
+            throw new IllegalArgumentException("Not enough available seats");
+        }
+        bookedSeats += seats;
+    }
+
+    public void cancelBooking(int seats) throws IllegalArgumentException {
+        if (this.date.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("You cannot cancel a booking for a past event");
+        }
+
+        if (seats <=0){
+            throw new IllegalArgumentException("Number of seats must be positive. You entered: " + seats);
+        }
+
+        if (bookedSeats - seats < 0){
+            throw new IllegalArgumentException("Not enough booked seats");
+        }
+        bookedSeats -= seats;
+    }
 }
